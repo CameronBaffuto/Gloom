9c9fe2a0fd9ed5b03a74260ce5a139024411586c
Saffat Hasan <saffathasan@gmail.com>
2020-02-25 19:57:28 -0500

    WIP

---
+++ web/app/src/Perks/perks.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import './App.css';
+
+class Perks extends Component {
+    render() {
+        return (
+            <div>Hello world!</div>
+        );
+    }
+}
+
+export default Perks;
--- web/app/src/index.js
+++ web/app/src/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import CharacterSelect from "./CharacterSelect";
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<CharacterSelect />, document.getElementById('root'));
+ReactDOM.render(<Perks/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
